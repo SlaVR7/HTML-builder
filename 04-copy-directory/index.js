91b81f8d1faa94db52d1bc3df27e4c9818e3dfd5
SlaVR <mylife0793@gmail.com>
2023-05-07 12:13:00 +0300

    feat: add task 4 solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,38 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir() {
+
+  fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+    if (err) throw err;
+
+    files.forEach(file => {
+      fs.unlink(path.join(path.join(__dirname, 'files-copy'), file), err => {
+        if (err) throw err;
+      });
+    });
+  });
+
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, error => {
+    if (error) {
+      throw new Error('error');
+    }
+    process.stdout.write('folder created ');
+    fs.readdir(path.join(__dirname, 'files'), (error, files) => {
+      if (error) {
+        throw new Error('error');
+      }
+      files.forEach(file => {
+        const filePath = path.join(__dirname, 'files', file);
+        fs.copyFile(filePath, path.join(__dirname, 'files-copy', file), error => {
+          if (error) {
+            throw new Error('error');
+          }
+          process.stdout.write(`${file} `);
+        });
+      });
+    });
+  });
+}
+
+copyDir();
